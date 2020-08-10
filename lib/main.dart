e2fdecf12ea44017354a51857ef3d0132a41d2ed
mohamed tarek <65023914+Mohamed-Tarek1@users.noreply.github.com>
2020-08-10 14:12:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'item.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -11,7 +11,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
+      home: Scaffold( body: Item(images: ['https://cdn.pixabay.com/photo/2018/08/18/09/51/fashion-3614477_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/08/18/09/51/fashion-3614477_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2018/08/18/09/51/fashion-3614477_960_720.jpg'],price: 3000,) //HomePage(address: 'Cairo, Attaba',shop_name: 'Shop Name',type: 'Desses',url: NetworkImage('https://picsum.photos/250?image=9')),
+      ));
   }
 }
